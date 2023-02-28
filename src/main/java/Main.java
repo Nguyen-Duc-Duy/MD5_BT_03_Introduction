4afab94e1392b8d92e51f3167bc94a3339e026f8
DuyNDisDev <49187501+Nguyen-Duc-Duy@users.noreply.github.com>
2023-02-28 17:12:43 +0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,166 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("vui lòng nhập số cần đọc");
+        Scanner scanner = new Scanner(System.in);
+        short number = scanner.nextShort();
+        int ones = (number%10);
+        String textOnes = "";
+        int tens = (number%100)/10;
+        String textTens = "";
+        int hundreds = number/100;
+        String texthundreds = "";
+        System.out.println("hundreds: "+hundreds+", tens "+tens+",one "+ones);
+        String resoult="";
+        switch (ones){
+            case 0:
+                textOnes = "zero";
+                break;
+            case 1:
+                textOnes = "one";
+                break;
+            case 2:
+                textOnes = "two";
+                break;
+            case 3:
+                textOnes = "three";
+                break;
+            case 4:
+                textOnes = "four";
+                break;
+            case 5:
+                textOnes = "five";
+                break;
+            case 6:
+                textOnes = "six";
+                break;
+            case 7:
+                textOnes = "seven";
+                break;
+            case 8:
+                textOnes = "eight";
+                break;
+            case 9:
+                textOnes = "nine";
+                break;
+            default:
+                System.out.println("out of ability");
+        }
+
+        if(tens==1){
+            tens = Integer.parseInt(tens+""+ones);
+            System.out.println(tens);
+            switch (tens){
+                case 10:
+                    textOnes = "";
+                    textTens = "Ten ";
+                    break;
+                case 11:
+                    textOnes = "";
+                    textTens = "Eleven ";
+                    break;
+                case 12:
+                    textOnes = "";
+                    textTens = "Twelve ";
+                    break;
+                case 13:
+                    textOnes = "";
+                    textTens = "Thirteen ";
+                    break;
+                case 14:
+                    textOnes = "";
+                    textTens = "Fourteen ";
+                    break;
+                case 15:
+                    textOnes = "";
+                    textTens = "Fifteen ";
+                    break;
+                case 16:
+                    textOnes = "";
+                    textTens = "Sixteen ";
+                    break;
+                case 17:
+                    textOnes = "";
+                    textTens = "Seventeen ";
+                    break;
+                case 18:
+                    textOnes = "";
+                    textTens = "Eighteen ";
+                    break;
+                case 19:
+                    textOnes = "";
+                    textTens = "Nineteen ";
+                    break;
+            }
+        }
+        else if(tens>=2&&tens<10){
+            switch (tens){
+                case 2:
+                    textTens = "Twenty";
+                    break;
+                case 3:
+                    textTens = "Thirty";
+                    break;
+                case 4:
+                    textTens = "Forty";
+                    break;
+                case 5:
+                    textTens = "Fifty";
+                    break;
+                case 6:
+                    textTens = "Sixty";
+                    break;
+                case 7:
+                    textTens = "Seventy";
+                    break;
+                case 8:
+                    textTens = "Eighty";
+                    break;
+                case 9:
+                    textTens = "Ninety";
+                    break;
+            }
+        }
+
+        switch (hundreds){
+            case 0:
+                texthundreds = "";
+                break;
+            case 1:
+                texthundreds = "one";
+                break;
+            case 2:
+                texthundreds = "two";
+                break;
+            case 3:
+                texthundreds = "three";
+                break;
+            case 4:
+                texthundreds = "four";
+                break;
+            case 5:
+                texthundreds = "five";
+                break;
+            case 6:
+                texthundreds = "six";
+                break;
+            case 7:
+                texthundreds = "seven";
+                break;
+            case 8:
+                texthundreds = "eight";
+                break;
+            case 9:
+                texthundreds = "nine";
+                break;
+            default:
+                System.out.println("out of ability");
+        }
+
+        resoult = hundreds !=0 ? texthundreds+" Hundreds " : "";
+        resoult += ones == 0 ? textTens : textTens+" "+textOnes;
+
+        System.out.println(resoult);
+    }
+}
